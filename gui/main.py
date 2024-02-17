d832fccce5ce9a5a27d833b814f57875a1b8831c
EashanC23 <108442887+EashanC23@users.noreply.github.com>
2024-02-17 13:02:24 -0800

    refactoring gui

--- gui/main.py
+++ gui/main.py
@@ -1,96 +1,83 @@
-import tkinter as tk
-from tkinter import ttk
-import cv2
-from PIL import Image, ImageTk
-import threading
+import tkinter as tk                # python 3
+from tkinter import font as tkfont  # python 3
+from main import WebcamPage
 
-class WebcamApp:
-    def __init__(self, window, window_title):
-        self.window = window
-        self.window.title(window_title)
-        self.window.geometry("1440x810")
-        self.window.resizable(False, False)
+class SampleApp(tk.Tk):
 
-        self.cap = cv2.VideoCapture(0)
+    def __init__(self, window, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        # self.title(window_title)
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
-        # Create a frame for the buttons
-        self.button_frame = ttk.Frame(self.window)
-        self.button_frame.pack(fill=tk.X, side=tk.BOTTOM, expand=True)
-        
-        # Create the Start Webcam button
-        self.btn_start = ttk.Button(self.button_frame, text="Start Webcam", command=self.start_webcam)
-        self.btn_start.pack(side=tk.LEFT, padx=5, pady=5)
-        
-        # Create the Stop Webcam button
-        self.btn_stop = ttk.Button(self.button_frame, text="Stop Webcam", command=self.stop_webcam)
-        self.btn_stop.pack(side=tk.LEFT, padx=5, pady=5)
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        # Create the Stop Webcam button
-        self.btn_stop = ttk.Button(self.button_frame, text="Button 3", command=self.stop_webcam)
-        self.btn_stop.pack(side=tk.LEFT, padx=5, pady=5)
-        
-        # Create the Stop Webcam button
-        self.btn_stop = ttk.Button(self.button_frame, text="Exit", command=exit)
-        self.btn_stop.pack(side=tk.LEFT, padx=5, pady=5)
+        self.frames = {}
+        for F in (StartPage, PageOne, PageTwo, WebcamPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            self.window = self
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
 
-        self.video_frame = tk.Frame(self.window, width=640, height=480)
-        self.video_frame.pack(padx=10, pady=10, expand=True)
-        
-        # Adjustments to video label to place it inside video_frame
-        self.video_label = tk.Label(self.video_frame)
-        self.video_label.pack()
+        self.show_frame("StartPage")
 
-        # Timer label
-        self.timer_label = tk.Label(self.video_frame, text="Timer Text", bg="yellow", fg="black")
-        self.timer_label.place(relx=0.7, rely=0, relwidth=0.3, relheight=0.2)
-        self.timer_label.config(bg="yellow")
-        self.timer_label.config(fg="black")
-        self.timer_label.config(font=("Arial", 12))
-        
-        self.running = False
-        self.update_frame()  # Start the update loop for the video frames
-        
-        self.window.mainloop()
-    
-    def start_webcam(self):
-        if not self.running:
-            self.running = True
-    
-    def stop_webcam(self):
-        self.running = False
-        # Clear the video label
-        self.video_label.config(image='')  # Clears the label
-        # Optionally, you can set a placeholder text or a default image here
-        self.video_label.config(text='Webcam stopped')  # Placeholder text
-        # If setting a default image, you would do something like this:
-        default_img = ImageTk.PhotoImage(Image.open("./imgs/360_F_526665446_z51DM27QvvoMZ9Gkyx9gr5mkjSOmjswR.jpg"))
-        self.video_label.imgtk = default_img
-        self.video_label.config(image=default_img)
-    
-    def update_frame(self):
-        if self.running:
-            # Capture the latest frame from the webcam
-            ret, frame = self.cap.read()
-            if ret:
-                # Convert the image from BGR (OpenCV format) to RGB
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                # Convert the image to PIL format
-                img = Image.fromarray(frame)
-                # Convert the image for Tkinter
-                imgtk = ImageTk.PhotoImage(image=img)
-                # Update the label with the new image
-                self.video_label.imgtk = imgtk
-                self.video_label.configure(image=imgtk)
-        else:
-            default_img = ImageTk.PhotoImage(Image.open("./imgs/360_F_526665446_z51DM27QvvoMZ9Gkyx9gr5mkjSOmjswR.jpg"))
-            self.video_label.imgtk = default_img
-            self.video_label.config(image=default_img)
-        self.window.after(10, self.update_frame)  # Repeat after an interval
-        
-    def __del__(self):
-        if self.cap.isOpened():
-            self.cap.release()
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
-# Create a window and pass it to the WebcamApp class
-root = tk.Tk()
-app = WebcamApp(root, "Tkinter Webcam Integration")
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Go to Page One",
+                            command=lambda: controller.show_frame("PageOne"))
+        button2 = tk.Button(self, text="Go to Page Two",
+                            command=lambda: controller.show_frame("PageTwo"))
+        button1.pack()
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 1", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 2", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+if __name__ == "__main__":
+    root=tk.Tk()
+    app = SampleApp(root)
+    app.mainloop()
---
+++ gui/webcamPage.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+from tkinter import ttk
+import cv2
+from PIL import Image, ImageTk
+import threading
+
+class WebcamPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+       # Initialize webcam
+        self.cap = cv2.VideoCapture(0)
+
+        # Create a frame for the buttons
+        self.button_frame = ttk.Frame(self)
+        self.button_frame.pack(fill=tk.X, side=tk.BOTTOM, expand=True)
+        
+        # Buttons
+        self.btn_start = ttk.Button(self.button_frame, text="Start Webcam", command=self.start_webcam)
+        self.btn_start.pack(side=tk.LEFT, padx=5, pady=5)
+        
+        self.btn_stop = ttk.Button(self.button_frame, text="Stop Webcam", command=self.stop_webcam)
+        self.btn_stop.pack(side=tk.LEFT, padx=5, pady=5)
+
+        # Create the Stop Webcam button
+        self.btn_stop = ttk.Button(self.button_frame, text="Button 3", command=self.stop_webcam)
+        self.btn_stop.pack(side=tk.LEFT, padx=5, pady=5)
+        
+        # Create the Stop Webcam button
+        self.btn_stop = ttk.Button(self.button_frame, text="Exit", command=exit)
+        self.btn_stop.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.video_frame = tk.Frame(self, width=640, height=480)
+        
+        # Adjustments to video label to place it inside video_frame
+        self.video_label = tk.Label(self.video_frame)
+        self.video_label.pack()
+
+        self.video_frame.pack(padx=10, pady=10, expand=True)
+
+        # Timer label
+        self.timer_label = tk.Label(self.video_frame, text="Timer Text", bg="yellow", fg="black")
+        self.timer_label.place(relx=0.7, rely=0, relwidth=0.3, relheight=0.2)
+        self.timer_label.config(bg="yellow")
+        self.timer_label.config(fg="black")
+        self.timer_label.config(font=("Arial", 12))
+        
+        self.running = False
+        self.update_frame()  # Start the update loop for the video frames
+        
+        self.window.mainloop()
+    
+    def start_webcam(self):
+        if not self.running:
+            self.running = True
+    
+    def stop_webcam(self):
+        self.running = False
+        # Clear the video label
+        self.video_label.config(image='')  # Clears the label
+        # Optionally, you can set a placeholder text or a default image here
+        self.video_label.config(text='Webcam stopped')  # Placeholder text
+        # If setting a default image, you would do something like this:
+        default_img = ImageTk.PhotoImage(Image.open("./imgs/360_F_526665446_z51DM27QvvoMZ9Gkyx9gr5mkjSOmjswR.jpg"))
+        self.video_label.imgtk = default_img
+        self.video_label.config(image=default_img)
+    
+    def update_frame(self):
+        if self.running:
+            # Capture the latest frame from the webcam
+            ret, frame = self.cap.read()
+            if ret:
+                # Convert the image from BGR (OpenCV format) to RGB
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # Convert the image to PIL format
+                img = Image.fromarray(frame)
+                # Convert the image for Tkinter
+                imgtk = ImageTk.PhotoImage(image=img)
+                # Update the label with the new image
+                self.video_label.imgtk = imgtk
+                self.video_label.configure(image=imgtk)
+        else:
+            default_img = ImageTk.PhotoImage(Image.open("./imgs/360_F_526665446_z51DM27QvvoMZ9Gkyx9gr5mkjSOmjswR.jpg"))
+            self.video_label.imgtk = default_img
+            print(default_img == None)
+            self.video_label.config(image=default_img)
+        self.window.after(10, self.update_frame)  # Repeat after an interval
+        
+    def __del__(self):
+        if self.cap.isOpened():
+            self.cap.release()
+
+# Create a window and pass it to the WebcamApp class
+# root = tk.Tk()
+# app = WebcamPage()
+# 

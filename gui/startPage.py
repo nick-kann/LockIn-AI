a140c94d12dd2089bef4819bb14553eac3743700
EashanC23 <108442887+EashanC23@users.noreply.github.com>
2024-02-18 01:40:36 -0800

    custom tkinter

--- gui/startPage.py
+++ gui/startPage.py
@@ -1,30 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
+import customtkinter as ctk
 
-class StartPage(tk.Frame):
+class StartPage(ctk.CTkFrame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        super().__init__(parent)
         self.controller = controller
         
-        label = ttk.Label(self, text="Welcome to Your App", font=controller.title_font)
+        label = ctk.CTkLabel(self, text="This is the start page", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         
-        # Styling for the buttons
-        style = ttk.Style()
-        style.configure('Main.TButton', font=('Lato', 50))
+        button1 = ctk.CTkButton(self, text="Focus Mode", command=lambda: controller.show_frame("WebcamPage"))
+        button2 = ctk.CTkButton(self, text="Graph your focus", command=lambda: controller.show_frame("GraphPage"))
+        button3 = ctk.CTkButton(self, text="Overlay Mode", command=lambda: controller.show_frame("OverlayPage"))
         
-        # Button 1: FOCUS MODE
-        button1 = ttk.Button(self, text="FOCUS MODE", style='Main.TButton',
-                            command=lambda: controller.show_frame("WebcamPage"))
-        button1.pack(pady=20, padx=50, ipadx=5, ipady=10, fill='x')
-        
-        # Button 2: GRAPH YOUR FOCUS
-        button2 = ttk.Button(self, text="GRAPH YOUR FOCUS", style='Main.TButton',
-                            command=lambda: controller.show_frame("GraphPage"))
-        button2.pack(pady=20, padx=50, ipadx=5, ipady=10, fill='x')
-        
-        # Button 3: OVERLAY MODE
-        button3 = ttk.Button(self, text="OVERLAY MODE", style='Main.TButton',
-                            command=lambda: controller.show_frame("OverlayPage"))
-        button3.pack(pady=20, padx=50, ipadx=5, ipady=10, fill='x')
+        button1.pack()
+        button2.pack()
+        button3.pack()
+

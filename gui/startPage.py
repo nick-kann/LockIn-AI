928e64332f4ec4f9af4fdaaa295dd20626def743
EashanC23 <108442887+EashanC23@users.noreply.github.com>
2024-02-17 19:23:10 -0800

    styling

--- gui/startPage.py
+++ gui/startPage.py
@@ -1,18 +1,19 @@
 import tkinter as tk   
+import ttkbootstrap as ttk
 
 class StartPage(tk.Frame):
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+    def __init__(self,  parent, controller):
+        tk.Frame.__init__(self, parent )
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        label = ttk.Label(self, text="This is the start page", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Focus Mode",
+        button1 = ttk.Button(self, text="Focus Mode", bootstyle="outline",
                             command=lambda: controller.show_frame("WebcamPage"))
-        button2 = tk.Button(self, text="Graph your focus",
+        button2 = ttk.Button(self, text="Graph your focus", bootstyle="outline", 
                             command=lambda: controller.show_frame("graphPage"))
-        button3 = tk.Button(self, text="Overlay Mode",
+        button3 = ttk.Button(self, text="Overlay Mode", bootstyle="outline",
                             command=lambda: controller.show_frame("OverlayPage"))
         button1.pack()
         button2.pack()
